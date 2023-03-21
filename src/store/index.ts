afb26782a36d7f7ffe90533a48f2a3b530f07623
aronaNg <arona.ngom07@gmail.com>
2023-03-21 23:51:47 +0100

    stop project

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,133 @@
+import {createStore, GetterTree, Store, useStore as baseUseStore,ActionTree, MutationTree} from "vuex"
+import { InjectionKey } from "vue";
+export type Category = {id:number, libelle:string, user_id?:number,createdAt?:string, updatedAt?:string}
+//export type ;
+export type State = { categories: Category[]/*,categories:Category[]*/ }
+import axios from "axios";
+
+
+const state: State = {
+    categories:[
+       
+    ]
+}
+
+
+const API_URL = "http://localhost:8082/api"
+
+const getters: GetterTree<State,State> = {
+    getAllTodos: (state) => {
+        return state.categories
+    },
+    getTodoById : (state) => (id:number) => state.categories.find(todo => todo.id === id)
+}
+
+const actions:ActionTree<State,State> ={
+    addTodo(context, newTodo) {
+        
+
+        return axios.post(API_URL + "/categories", newTodo).then((response) => {
+            
+            if (response.status == 200) {
+                context.commit("addTodo", newTodo);
+            }
+
+            return {"statut":response.status, "message":response.statusText}
+
+        }).catch((error) => {
+            console.log(error);
+            return {"statut":error.status,"message":error}
+        })
+
+    },
+    editTodo(context, editTodo) {
+        return axios.put(API_URL + "/categories/"+editTodo.id, editTodo).then((response) => {
+            
+            if (response.status == 200) {
+                context.commit("editTodo", editTodo);
+            }
+
+            return {"statut":response.status, "message":response.statusText}
+
+        }).catch((error) => {
+            console.log(error);
+            return {"statut":error.status,"message":error}
+        })
+    },
+    deleteTodo(context, todo_id) {
+        return axios.delete(API_URL + "/categories/"+todo_id,).then((response) => {
+            
+            if (response.status == 200) {
+                context.commit("deleteTodo", todo_id);
+            }
+
+            return {"statut":response.status, "message":response.statusText}
+
+        }).catch((error) => {
+            console.log(error);
+            return {"statut":error.status,"message":error}
+        })
+    },
+    async getTodos(state) {
+        const categories = await axios.get(API_URL+"/categories");
+
+        state.commit("GET_TODO",categories.data)
+    }
+}
+
+const mutations: MutationTree<State> = {
+    GET_TODO(state, categories) {
+        state.categories = categories;
+    },
+    addTodo(state,newTodo){
+        const todoFormated = {
+            libelle:newTodo.libelle,
+            id:state.categories.length
+        }
+
+        state.categories.push(todoFormated);
+    },
+
+    editTodo(state, editTodo) {
+        const index = state.categories.findIndex(todo => todo.id === editTodo.id)
+
+        state.categories[index] = editTodo
+
+    },
+
+    deleteTodo(state, todo_id) {
+        const index = state.categories.findIndex(todo => todo.id === todo_id)
+        console.log("delete index : ", index)
+        state.categories.splice(index,1)
+
+    }
+    
+}
+
+
+
+export const store = createStore<State>({state,getters,actions,mutations})
+
+export const key:InjectionKey<Store<State>>=Symbol();
+
+export function useStore(){
+    return baseUseStore(key);
+}
+
+
+
+// le store pour les concerts
+
 // import {createStore, GetterTree, Store, useStore as baseUseStore,ActionTree, MutationTree} from "vuex"
 // import { InjectionKey } from "vue";
-// export type Category = {id:number, libelle:string, user_id?:number,createdAt?:string, updatedAt?:string}
+// export type Concert = {id:number, nom:string, description:string,image?:string,date?:string,heure?:string,categorie_id?:number, user_id?:number,createdAt?:string, updatedAt?:string}
 // //export type ;
-// export type State = { categories: Category[]/*,categories:Category[]*/ }
+// export type State = { concerts: Concert[]}
 // import axios from "axios";
 
 
 // const state: State = {
-//     categories:[
+//     concerts:[
        
 //     ]
 // }
@@ -17,16 +137,16 @@
 
 // const getters: GetterTree<State,State> = {
 //     getAllTodos: (state) => {
-//         return state.categories
+//         return state.concerts
 //     },
-//     getTodoById : (state) => (id:number) => state.categories.find(todo => todo.id === id)
+//     getTodoById : (state) => (id:number) => state.concerts.find(todo => todo.id === id)
 // }
 
 // const actions:ActionTree<State,State> ={
 //     addTodo(context, newTodo) {
         
 
-//         return axios.post(API_URL + "/categories", newTodo).then((response) => {
+//         return axios.post(API_URL + "/concerts", newTodo).then((response) => {
             
 //             if (response.status == 200) {
 //                 context.commit("addTodo", newTodo);
@@ -41,7 +161,7 @@
 
 //     },
 //     editTodo(context, editTodo) {
-//         return axios.put(API_URL + "/categories/"+editTodo.id, editTodo).then((response) => {
+//         return axios.put(API_URL + "/concerts/"+editTodo.id, editTodo).then((response) => {
             
 //             if (response.status == 200) {
 //                 context.commit("editTodo", editTodo);
@@ -55,7 +175,7 @@
 //         })
 //     },
 //     deleteTodo(context, todo_id) {
-//         return axios.delete(API_URL + "/categories/"+todo_id,).then((response) => {
+//         return axios.delete(API_URL + "/concerts/"+todo_id,).then((response) => {
             
 //             if (response.status == 200) {
 //                 context.commit("deleteTodo", todo_id);
@@ -69,36 +189,42 @@
 //         })
 //     },
 //     async getTodos(state) {
-//         const categories = await axios.get(API_URL+"/categories");
+//         const concerts = await axios.get(API_URL+"/concerts");
 
-//         state.commit("GET_TODO",categories.data)
+//         state.commit("GET_TODO",concerts.data)
 //     }
 // }
 
 // const mutations: MutationTree<State> = {
-//     GET_TODO(state, categories) {
-//         state.categories = categories;
+//     GET_TODO(state, concerts) {
+//         state.concerts = concerts;
 //     },
 //     addTodo(state,newTodo){
 //         const todoFormated = {
-//             libelle:newTodo.libelle,
-//             id:state.categories.length
+//             id:state.concerts.length,
+//             nom:newTodo.nom,
+//             description:newTodo.description,
+//             image:newTodo.image,
+//             date:newTodo.date,
+//             heure:newTodo.heure,
+//             categorie_id:newTodo.categorie_id,
+//             user_id:newTodo.user_id,
 //         }
 
-//         state.categories.push(todoFormated);
+//         state.concerts.push(todoFormated);
 //     },
 
 //     editTodo(state, editTodo) {
-//         const index = state.categories.findIndex(todo => todo.id === editTodo.id)
+//         const index = state.concerts.findIndex(todo => todo.id === editTodo.id)
 
-//         state.categories[index] = editTodo
+//         state.concerts[index] = editTodo
 
 //     },
 
 //     deleteTodo(state, todo_id) {
-//         const index = state.categories.findIndex(todo => todo.id === todo_id)
+//         const index = state.concerts.findIndex(todo => todo.id === todo_id)
 //         console.log("delete index : ", index)
-//         state.categories.splice(index,1)
+//         state.concerts.splice(index,1)
 
 //     }
     
@@ -115,127 +241,120 @@
 // }
 
 
+// store pour user
 
-// le store pour les concerts
-
-import {createStore, GetterTree, Store, useStore as baseUseStore,ActionTree, MutationTree} from "vuex"
-import { InjectionKey } from "vue";
-export type Concert = {id:number, nom:string, description:string,image?:string,date?:string,heure?:string,categorie_id?:number, user_id?:number,createdAt?:string, updatedAt?:string}
-//export type ;
-export type State = { concerts: Concert[]}
-import axios from "axios";
+// import {createStore, GetterTree, Store, useStore as baseUseStore,ActionTree, MutationTree} from "vuex"
+// import { InjectionKey } from "vue";
+// export type User = {id:number, login:string, password:string, role:string,createdAt?:string, updatedAt?:string}
+// export type State = { users: User[] }
+// import axios from "axios";
 
 
-const state: State = {
-    concerts:[
+
+// const state: State = {
+//     users:[ 
        
-    ]
-}
+//     ]
+// }
 
 
-const API_URL = "http://localhost:8082/api"
+// const API_URL = "http://localhost:8082/api"
 
-const getters: GetterTree<State,State> = {
-    getAllTodos: (state) => {
-        return state.concerts
-    },
-    getTodoById : (state) => (id:number) => state.concerts.find(todo => todo.id === id)
-}
+// const getters: GetterTree<State,State> = {
+//     getAllTodos: (state) => {
+//         return state.users
+//     },
+//     getTodoById : (state) => (id:number) => state.users.find(todo => todo.id === id)
+// }
 
-const actions:ActionTree<State,State> ={
-    addTodo(context, newTodo) {
+// const actions:ActionTree<State,State> ={
+//     addTodo(context, newTodo) {
         
 
-        return axios.post(API_URL + "/concerts", newTodo).then((response) => {
+//         return axios.post(API_URL + "/user", newTodo).then((response) => {
             
-            if (response.status == 200) {
-                context.commit("addTodo", newTodo);
-            }
+//             if (response.status == 200) {
+//                 context.commit("addTodo", newTodo);
+//             }
 
-            return {"statut":response.status, "message":response.statusText}
+//             return {"statut":response.status, "message":response.statusText}
 
-        }).catch((error) => {
-            console.log(error);
-            return {"statut":error.status,"message":error}
-        })
+//         }).catch((error) => {
+//             console.log(error);
+//             return {"statut":error.status,"message":error}
+//         })
 
-    },
-    editTodo(context, editTodo) {
-        return axios.put(API_URL + "/concerts/"+editTodo.id, editTodo).then((response) => {
+//     },
+//     editTodo(context, editTodo) {
+//         return axios.put(API_URL + "/user/"+editTodo.id, editTodo).then((response) => {
             
-            if (response.status == 200) {
-                context.commit("editTodo", editTodo);
-            }
+//             if (response.status == 200) {
+//                 context.commit("editTodo", editTodo);
+//             }
 
-            return {"statut":response.status, "message":response.statusText}
+//             return {"statut":response.status, "message":response.statusText}
 
-        }).catch((error) => {
-            console.log(error);
-            return {"statut":error.status,"message":error}
-        })
-    },
-    deleteTodo(context, todo_id) {
-        return axios.delete(API_URL + "/concerts/"+todo_id,).then((response) => {
+//         }).catch((error) => {
+//             console.log(error);
+//             return {"statut":error.status,"message":error}
+//         })
+//     },
+//     deleteTodo(context, todo_id) {
+//         return axios.delete(API_URL + "/user/"+todo_id,).then((response) => {
             
-            if (response.status == 200) {
-                context.commit("deleteTodo", todo_id);
-            }
+//             if (response.status == 200) {
+//                 context.commit("deleteTodo", todo_id);
+//             }
 
-            return {"statut":response.status, "message":response.statusText}
+//             return {"statut":response.status, "message":response.statusText}
 
-        }).catch((error) => {
-            console.log(error);
-            return {"statut":error.status,"message":error}
-        })
-    },
-    async getTodos(state) {
-        const concerts = await axios.get(API_URL+"/concerts");
+//         }).catch((error) => {
+//             console.log(error);
+//             return {"statut":error.status,"message":error}
+//         })
+//     },
+//     async getTodos(state) {
+//         const users = await axios.get(API_URL+"/user");
 
-        state.commit("GET_TODO",concerts.data)
-    }
-}
+//         state.commit("GET_TODO",users.data)
+//     }
+// }
 
-const mutations: MutationTree<State> = {
-    GET_TODO(state, concerts) {
-        state.concerts = concerts;
-    },
-    addTodo(state,newTodo){
-        const todoFormated = {
-            id:state.concerts.length,
-            nom:newTodo.nom,
-            description:newTodo.description,
-            image:newTodo.image,
-            date:newTodo.date,
-            heure:newTodo.heure,
-            categorie_id:newTodo.categorie_id,
-            user_id:newTodo.user_id,
-        }
+// const mutations: MutationTree<State> = {
+//     GET_TODO(state, users) {
+//         state.users = users;
+//     },
+//     addTodo(state,newTodo){
+//         const todoFormated = {
+//             login:newTodo.login,
+//             password:newTodo.password,
+//             role:newTodo.role,
+//             id:state.users.length
+//         }
 
-        state.concerts.push(todoFormated);
-    },
+//         state.users.push(todoFormated);
+//     },
 
-    editTodo(state, editTodo) {
-        const index = state.concerts.findIndex(todo => todo.id === editTodo.id)
+//     editTodo(state, editTodo) {
+//         const index = state.users.findIndex(todo => todo.id === editTodo.id)
 
-        state.concerts[index] = editTodo
+//         state.users[index] = editTodo
 
-    },
+//     },
 
-    deleteTodo(state, todo_id) {
-        const index = state.concerts.findIndex(todo => todo.id === todo_id)
-        console.log("delete index : ", index)
-        state.concerts.splice(index,1)
+//     deleteTodo(state, todo_id) {
+//         const index = state.users.findIndex(todo => todo.id === todo_id)
+//         console.log("delete index : ", index)
+//         state.users.splice(index,1)
 
-    }
+//     }
     
-}
+// }
 
+// export const store = createStore<State>({state,getters,actions,mutations})
 
+// export const key:InjectionKey<Store<State>>=Symbol();
 
-export const store = createStore<State>({state,getters,actions,mutations})
-
-export const key:InjectionKey<Store<State>>=Symbol();
-
-export function useStore(){
-    return baseUseStore(key);
-}
+// export function useStore(){
+//     return baseUseStore(key);
+// }
